47de35e17c1f1d3e9372565268720172e39ab15b
Nicol Wilcox <nwilcoxnz@gmail.com>
2015-07-09 22:48:51 +1200

    First commit

---
+++ app/src/main/java/com/example/nicolewilcox/flash/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.nicolewilcox.flash;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void viewSet(View v){
+        noCards(v);
+    }
+
+    public void noCards(View v){
+        AlertDialog.Builder helpDialog = new AlertDialog.Builder(this);
+        helpDialog.setMessage("You don't seem to have any cards! Make some by clicking \"new set\".")
+                .setPositiveButton("Got it!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .create();
+        helpDialog.show();
+    }
+
+    public void onClickHelp(View v){
+
+    }
+}
